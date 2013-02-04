9c0d962b9fe6e954a8ff5fcebececccb15fbef1a
nemono77 <nemono77@gmail.com>
2013-02-04 16:46:26 -0600

    Added foo1

--- GitTest/src/com/test/Test.java
+++ GitTest/src/com/test/Test.java
@@ -6,8 +6,13 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		foo1();
+	}
 
+	private static void foo1() {
+		for (int i=0; i < 5; i++)
+			System.out.println(i);
+		
 	}
 
 }
